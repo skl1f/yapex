f71a4297296f9b85c7fc2af0bac317805e6b8b9e
Алексей Мирошниченко < >
2016-01-18 23:44:25 +0200

    Placeholder for addon.py and language parsing from ex.ua

---
+++ ex.py
@@ -0,0 +1,19 @@
+import urllib2
+import xml.etree.ElementTree as ET
+
+def get_languages():
+    ```
+    return a dict with all awailable languages and with urls to rss
+    ```
+    page_url = 'http://www.ex.ua/rss/81708'
+    items = dict()
+    try:
+        page = urllib2.urlopen(page_url)
+        string = ''.join(page.readlines())
+        root = ET.fromstring(string)
+        all_items = root[0].findall('item')
+        for x in all_items:
+            items[x[0].text] = 'http://www.ex.ua/rss/{0}'.format(x[4].text)
+    except Exception, e:
+        pass
+    return items
